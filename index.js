8da93a654824443fab440d0b8d82fed9035ce391
Marlen Brunner <klondikemarlen@gmail.com>
2020-07-06 23:16:01 -0700

    :sparkles: Allow returning mocked up allowed time slots.

--- index.js
+++ index.js
@@ -3,21 +3,19 @@ const express = require("express");
 const basicAuth = require("express-basic-auth");
 const path = require("path");
 
+global.APP_ROOT = Object.freeze(path.dirname(__filename));
+
 const app = express();
 
 const listenPort = 4000;
 
-currentDir = path.dirname(__filename);
-
 const {
-  loadProviderInfo,
-  saveProviderInfo,
-  loadSchema,
+  loadTimeSlotsDefaults,
   requester,
   protocol,
   hostname,
   port,
-} = require(path.resolve(currentDir, "utils/helpers"));
+} = require(path.resolve(APP_ROOT, "utils/helpers"));
 
 const DAYS = Object.freeze({
   SUNDAY: 0,
@@ -48,6 +46,56 @@ app.get("/", (req, res) =>
       '<p>To read more on how to use this example app, visit <a href="https://github.com/proofgov/example-form-query-api">Proof\'s github form query api example page.</a>'
   )
 );
+
+app.get("/available-time-slots", (request, response) => {
+  console.log("request.url", request.url);
+  console.log("request", request.query);
+
+  const { building, floor: queryFloor, date: queryDate } = request.query;
+  const floor = Number(queryFloor);
+  const accessRequestedFor = new Date(queryDate);
+
+  console.log("building", building);
+  console.log("floor", floor);
+  console.log("accessRequestedFor", accessRequestedFor);
+
+  let data = {
+    availableTimeSlots: timeSlotDefaults,
+    info:
+      "Access is restricted:\n" +
+      "\t - after noon if date is a Tuesday (e.g. 2020/07/07)\n" +
+      "\t - accessing floor 13 will remove 9 am from the list.\n",
+  };
+
+  const DUMMY_DATE = "2020/07/06";
+  if (accessRequestedFor.getDay() !== DAYS.TUESDAY) {
+    data.availableTimeSlots = timeSlotDefaults.filter(
+      (label, value) => new Date(`${DUMMY_DATE} ${value}`).getHours() > 12
+    );
+  }
+
+  if (floor === 13) {
+    data.availableTimeSlots = timeSlotDefaults.filter(
+      (label, value) => new Date(`${DUMMY_DATE} ${value}`).getHours() !== 9
+    );
+  }
+
+  console.log(
+    "response:",
+    JSON.stringify(
+      {
+        status: 200,
+        type: "application/json",
+        data: data,
+      },
+      null,
+      2
+    )
+  );
+  response.type("application/json");
+  response.status(200);
+  response.send(data);
+});
 
 // building name, floor, request date and request time as parameters.
 //Have it return "blocked" if the request is noon or Tuesday, otherwise it says accept.
