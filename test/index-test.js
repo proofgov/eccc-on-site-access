944c82740520a1bbf802e2eeac37b7b0f27dffa0
Marlen Brunner <klondikemarlen@gmail.com>
2020-07-07 17:49:27 -0700

    :sparkles: Separate app container from test container.

--- test/index-test.js
+++ test/index-test.js
@@ -1,31 +1,29 @@
-const axios = require("axios");
+const axios = require('axios')
 
-describe("index.js", () => {
-  describe("#app", () => {
-    context("when querying the /is-time-available api", () => {
-      context("when no args passed", () => {
-        it("return that the time is available", () => {
+describe('index.js', () => {
+  describe('#app', () => {
+    context('when querying the /is-time-available api', () => {
+      context('when no args passed', () => {
+        it('return that the time is available', () => {
           return axios
-            .get("http://localhost:4000/is-time-available")
-            .then((response) =>
-              expect(response.data).to.include({ isAvailable: true })
-            );
-        });
+            .get('http://app:4000/is-time-available')
+            .then(response => expect(response.data).to.include({ isAvailable: true }))
+        })
 
-        it("always returns success", () => {
+        it('always returns success', () => {
           return axios
-            .get("http://localhost:4000/is-time-available")
-            .then((response) => expect(response.status).to.eq(200));
-        });
-      });
-    });
+            .get('http://app:4000/is-time-available')
+            .then(response => expect(response.status).to.eq(200))
+        })
+      })
+    })
 
-    context("when hitting app root", () => {
-      it("finds the welcome page", () => {
+    context('when hitting app root', () => {
+      it('finds the welcome page', () => {
         return axios
-          .get("http://localhost:4000")
-          .then((response) => expect(response.data).to.include("Welcome"));
-      });
-    });
-  });
-});
+          .get('http://app:4000')
+          .then(response => expect(response.data).to.include('Welcome'))
+      })
+    })
+  })
+})
